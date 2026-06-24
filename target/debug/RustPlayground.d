a903e8b5b1d26451d3643e12909f05e110f7df89
MajkiiWasTaken <msvrcek15github@gmail.com>
2026-06-24 13:23:01 +0200

    Init

---
+++ target/debug/RustPlayground.d
@@ -0,0 +1,1 @@
+C:\Users\SvrcekM\Documents\Project\RustPlayground\target\debug\RustPlayground.exe: C:\Users\SvrcekM\Documents\Project\RustPlayground\src\main.rs
---
+++ target/debug/deps/RustPlayground.d
@@ -0,0 +1,5 @@
+C:\Users\SvrcekM\Documents\Project\RustPlayground\target\debug\deps\RustPlayground.d: src\main.rs
+
+C:\Users\SvrcekM\Documents\Project\RustPlayground\target\debug\deps\RustPlayground.exe: src\main.rs
+
+src\main.rs:
